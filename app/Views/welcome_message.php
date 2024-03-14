4c4d14028ae53a5c33b66e3a199067fb830fa127
samy passu vargas <samypassu10@gmail.com>
2024-03-14 12:05:14 -0500

    proyecto inicializado

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to CodeIgniter 4!</title>
+    <meta name="description" content="sistema para el control e inventario de equipo">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">        
+</head>
+<body>
+
+</body>
+</html>
