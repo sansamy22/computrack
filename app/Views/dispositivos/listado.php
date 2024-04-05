9a38b97ae00a3439e7c66766b0f528d969c6b582
sansamy22 <160153145+sansamy22@users.noreply.github.com>
2024-04-04 21:28:31 -0500

    pagina terminada, para revisar visual revisar 'app.php'

--- app/Views/dispositivos/listado.php
+++ app/Views/dispositivos/listado.php
@@ -2,49 +2,38 @@
 
 <?= $this->section('contenido') ?>
 
-<h1>Listado de Dispositivos</h1> 
+<div class="container">
+    <h1>Listado de Dispositivos</h1>
+    <a class="btn btn-success mb-3" href="<?= base_url('dispositivos/nuevo') ?>">Nuevo Dispositivo</a>
 
-<a class="btn btn-success" href="<?= base_url('dispositivos/nuevo')?>">Nuevo Dispositivo</a>
+    <div class="table-responsive">
+        <table class="table table-hover table-sm">
+            <thead>
+                <tr>
+                    <th>Tipo Dispositivo</th>
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>Color</th>
+                    <th>Serial</th>
+                    <th>Diagnóstico</th>
+                    <th>Cliente</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($dispositivos as $dispositivo) : ?>
+                    <tr>
+                        <td><?= $dispositivo['tipoDispositivo'] ?></td>
+                        <td><?= $dispositivo['marca'] ?></td>
+                        <td><?= $dispositivo['modelo'] ?></td>
+                        <td><?= $dispositivo['color'] ?></td>
+                        <td><?= $dispositivo['serial'] ?></td>
+                        <td><?= $dispositivo['diagnostico'] ?></td>
+                        <td><?= $dispositivo['nombreCompleto'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
 
-
-<table class="table table-hover table-sm">
-    <thead>
-        <th>Tipo de dispositivo</th>
-        <th>Marca</th>
-        <th>Modelo</th>
-        <th>color</th>
-        <th>Serial</th>
-        <th>diagnostico</th>
-        <th>Cliente</th>
-    </thead>
-    <tbody>
-        <?php
-        foreach ($dispositivos as $dispositivo) :
-        ?>
-            <tr>
-            <td>
-                    <?= $dispositivo['tipoDispositivo'] ?>
-                </td>
-                <td>
-                    <?= $dispositivo['marca'] ?>
-                </td>
-                <td>
-                    <?= $dispositivo['modelo'] ?>
-                </td>
-                <td>
-                    <?= $dispositivo['color'] ?>
-                </td>
-                <td>
-                    <?= $dispositivo['serial'] ?>
-                </td>
-                <td>
-                    <?= $dispositivo['diagnostico'] ?>
-                </td>
-                <td>
-                    <?= $dispositivo['nombreCompleto'] ?>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
 <?= $this->endSection() ?>
--- app/Views/ordenes/nueva.php
+++ app/Views/ordenes/nueva.php
@@ -2,39 +2,44 @@
 
 <?= $this->section('contenido') ?>
 
-<h1>Nueva orden </h1>
-
 <div class="container">
+    <h1>Nueva orden</h1>
     <div class="row">
         <div class="col-lg-4">
             <div class="form-group">
                 <label for="">Cédula</label>
                 <input type="text" id="cedula" class="form-control" autocomplete="off">
-                <button id="btnBuscarCliente" class="btn btn-primary">Buscar cliente</button>
+                <button id="btnBuscarCliente" class="btn btn-primary mt-2">Buscar cliente</button>
             </div>
         </div>
-        <div class="col-lg-8">
+        <div class="col-lg-8 mt-lg-0 mt-3">
             <h3 id="nombreCliente"></h3>
         </div>
     </div>
     <div class="row mt-4">
-        <table id="tablaDispositivos" class="table table-hover table-sm table-primary">
-            <thead>
-                <th>Tipo de Dispositivo</th>
-                <th>Marca</th>
-                <th>Modelo</th>
-                <th>Color</th>
-                <th>Serial</th>
-                <th>Acciones</th>
-            </thead>
-            <tbody>
-                <!-- Aquí se llenarán los datos dinámicamente -->
-            </tbody>
-        </table>
+        <div class="col">
+            <div class="table-responsive">
+                <table id="tablaDispositivos" class="table table-hover table-sm table-primary">
+                    <thead>
+                        <tr>
+                            <th>Tipo de Dispositivo</th>
+                            <th>Marca</th>
+                            <th>Modelo</th>
+                            <th>Color</th>
+                            <th>Serial</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- Aquí se llenarán los datos dinámicamente -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
     <form action="<?= site_url('ordenes/guardarOrden') ?>" method="post" id="formularioOrden">
-        <div class="row">
+        <div class="row mt-4">
             <div class="col-lg-6">
                 <div class="form-group">
                     <label for="">Observaciones</label>
@@ -44,7 +49,7 @@
             <div class="col-lg-6">
                 <div class="form-group">
                     <label for="">Dispositivo</label>
-                    <span class="badge text-bg-info" id="dispositivoSeleccionado"></span>
+                    <span class="badge bg-info text-white" id="dispositivoSeleccionado"></span>
                     <!-- Agregar un campo oculto para almacenar el ID del dispositivo -->
                     <input type="hidden" name="dispositivo_id" id="dispositivo_id">
                 </div>
@@ -57,8 +62,8 @@
 </div>
 
 <script>
-    $(document).ready(function () {
-        $('table#tablaDispositivos tbody').on('click', 'button.btn-success', function () {
+    $(document).ready(function() {
+        $('table#tablaDispositivos tbody').on('click', 'button.btn-success', function() {
             let idDispositivo = $(this).closest('tr').attr('data-id');
             let tipoDispositivo = $(this).closest('tr').find('td:eq(0)').text();
             let serial = $(this).closest('tr').find('td:eq(4)').text();
@@ -71,7 +76,7 @@
         });
 
         // Evento de clic en el botón "Buscar cliente"
-        $('button#btnBuscarCliente').on('click', function () {
+        $('button#btnBuscarCliente').on('click', function() {
             let cedula = $('input#cedula').val();
 
             // Enviar la solicitud AJAX al controlador para buscar los dispositivos asociados al cliente
@@ -82,7 +87,7 @@
                 data: {
                     cedula: cedula
                 },
-                success: function (response) {
+                success: function(response) {
                     if (response.success) {
                         // Mostrar el nombre del cliente
                         $('h3#nombreCliente').text(response.cliente.nombres + ' ' + response.cliente.apellidos);
@@ -91,7 +96,7 @@
                         $('table#tablaDispositivos tbody').empty();
 
                         // Mostrar los dispositivos asociados al cliente en la tabla
-                        $.each(response.dispositivos, function (index, dispositivo) {
+                        $.each(response.dispositivos, function(index, dispositivo) {
                             $('table#tablaDispositivos tbody').append('<tr data-id="' + dispositivo.id + '"><td>' + dispositivo.tipoDispositivo + '</td><td>' + dispositivo.marca + '</td><td>' + dispositivo.modelo + '</td><td>' + dispositivo.color + '</td><td>' + dispositivo.serial + '</td><td><button class="btn btn-success">Seleccionar</button></td></tr>');
                         });
                     } else {
@@ -99,7 +104,7 @@
                         alert(response.message);
                     }
                 },
-                error: function () {
+                error: function() {
                     // Si ocurrió un error en la solicitud AJAX, mostrar un mensaje de error
                     alert('Error al buscar dispositivo');
                 }
